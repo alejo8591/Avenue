e1dfd5187669ab5d0578f9083fb9ab6f3507428b
Alejandro Romero <alejo8591@gmail.com>
2013-09-20 13:19:59 -0500

    update header.php with google analytics tracking code

--- header.php
+++ header.php
@@ -25,6 +25,17 @@ if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 wp_head(); 
 ?>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44206438-1', 'westhouseinmobiliaria.com');
+  ga('send', 'pageview');
+
+</script>
+
 </head>
 <body>
 
